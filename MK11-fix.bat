805f4f4a03fd12876a6a88ea7aa99441a1ebc48d
pixellockerchik <quappiz@tuta.io>
2025-04-28 22:02:47 +0300

    add logs file and other changes

--- MK11-fix.bat
+++ MK11-fix.bat
@@ -1,19 +1,12 @@
-@echo off
-
-:: Проверка прав файла на права администратора
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Требуются права администратора! Запустите файл от имени админа.
-    pause
-    exit /b
-)
-
+(@echo off
+chcp 1251 > nul
 :: Удаляем  прошлую папку с сертификатами
-powershell Remove-Item -Path "$Env:TEMP\certs" -Recurse -Force 
+powershell -WindowStyle Hidden Remove-Item -Path "$Env:TEMP\certs" -Recurse -Force 
 
 :: Начало работы самих команд для решения проблем с MK11
-powershell md $Env:TEMP\certs
-powershell cd $Env:TEMP\certs
-powershell CertUtil -generateSSTFromWU $Env:TEMP\certs\RootStore.sst
-powershell $file=Get-ChildItem -Path $Env:TEMP\certs\Rootstore.sst
-powershell $file | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root
+powershell  md $Env:TEMP\certs
+powershell  cd $Env:TEMP\certs
+powershell  CertUtil -generateSSTFromWU $Env:TEMP\certs\RootStore.sst
+powershell  $file=Get-ChildItem -Path $Env:TEMP\certs\Rootstore.sst
+powershell  $file | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root
+) > "log.txt"
